8fa30c3eb63eae528bf94b872a22e98fa1f4c925
Kyounga <kacomtr@gamil.com>
2025-02-20 01:22:26 +0900

    리디렉트 오류

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
     if (router.pathname === '/') {
       router.replace('/main');
     }
-  }, [router.pathname]);
+  }, []);
 
   return (
     <>

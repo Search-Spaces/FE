4771013877aab5097a434b4a42def58b50ff5053
Kyounga <kacomtr@gamil.com>
2025-02-20 23:11:45 +0900

    map페이지 로드안되는 문제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Nav from '../components/Navigation';
 import Layout from '../layouts/layout';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   // console.log('NAVER_CLIENT_ID:', process.env.NEXT_PUBLIC_NAVER_CLIENT_ID);
@@ -13,20 +14,24 @@ export default function App({ Component, pageProps }: AppProps) {
     router.pathname === '/login' ||
     router.pathname === '/';
 
-  useEffect(()=> {
-    const handleRouteChange = (url: string)=>{
-      if (url === '/map' && window.location.pathname === '/map'){
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (url === '/map' && window.location.pathname === '/map') {
         window.location.reload();
       }
     };
-    router.events.on('routeChangeComplete',handleRouteChange);
-    return ()=> {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
       router.events.off('routeChangeComplete', handleRouteChange);
-    }
-  }, [router])
+    };
+  }, [router]);
 
   return (
     <>
+      <Script
+        strategy="beforeInteractive"
+        src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&submodules=geocoder`}
+      />
       <Layout>
         {!hideNav && <Nav />}
         <main style={{ flex: 1 }}>

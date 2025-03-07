791e6b488275d0aa3c15bd28a56cbfe2f210a573
kyounga <kacomtr@gmail.com>
2025-03-07 19:30:11 +0900

    가장 최신버전으로 되돌리기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,45 @@
-export { default } from './main';
+'use client';
+import styles from './main.module.css';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+const Main = () => {
+  const router = useRouter();
+  const onClickLogin = () => {
+    router.push('/login');
+  };
+  const onClickGuest = () => {
+    router.push('/map');
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <div className={styles.titleContainer}>
+          <Image
+            src="/title.png"
+            alt="title"
+            className={styles.titleImage}
+            fill
+          />
+        </div>
+        <div className={styles.doormarkContainer}>
+          <Image
+            src="/doormark.png"
+            alt="title"
+            className={styles.doormarkImage}
+            fill
+          />
+        </div>
+      </div>
+      <div className={styles.buttonContainer}>
+        <button className={styles.loginButton} onClick={onClickLogin}>
+          로그인하기
+        </button>
+        <button className={styles.startButton} onClick={onClickGuest}>
+          게스트로 둘러보기
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- pages/main.tsx
+++ pages/main.tsx
@@ -1,4 +1,4 @@
-import styles from './main.module.css';
+/*import styles from './main.module.css';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 const Main = () => {
@@ -42,3 +42,4 @@ const Main = () => {
 };
 
 export default Main;
+*/
